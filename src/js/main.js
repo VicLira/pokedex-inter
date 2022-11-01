f9d0e3bc8ca99e30f9e3efbdbc91b0495e0580ce
Victor <victor.liracarlos@gmail.com>
2022-11-01 19:41:38 -0300

    Criada api para listagem de pokemons

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,28 @@
 import "../css/global.css"
+import "./poke-api.js"
 
-const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
+function convertPokemonToHtml(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+          <div class="detail">
+            <ol class="types">
+              <li class="type">grass</li>
+              <li class="type">poison</li>
+            </ol>
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              alt="${pokemon.name}"
+            />
+        </div>
+      </li>
+  `
+}
 
-fetch(url)
-  .then((response) => {
-    return response.json()
-  })
-  .then((jsonBody) => {
-    console.log(jsonBody)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-  .finally(() => {
-    console.log("Requisição concluída!")
-  })
+const pokemonOlList = document.getElementById("pokemon-list")
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonOlList.innerHTML = pokemons.map(convertPokemonToHtml).join("")
+  console.log(newList)
+})
